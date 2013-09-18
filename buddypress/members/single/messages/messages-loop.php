21059a6b47c10c7bc542e42e03f8319a1614e564
hnla <hugoashmore@tiscali.co.uk>
2013-09-18 10:54:32 +0100

    First pass for messages screens re-factoring, re-configures messages.php with new switches, adds markup and scripting for messages-loop.php and single.php and test data for compose screen.

--- buddypress/members/single/messages/messages-loop.php
+++ buddypress/members/single/messages/messages-loop.php
@@ -6,4 +6,38 @@
  * @subpackage Templatepack
  */
 ?>
-<!-- to come -->
+<?php do_action( 'bp_before_member_messages_loop' ); ?>
+
+<?php if ( bp_has_message_threads( bp_ajax_querystring( 'messages' ) ) ) : ?>
+
+<div class="messages-list">
+
+	<ul>
+	
+	<?php while ( bp_message_threads() ) : bp_message_thread(); ?>		
+		
+		<li id="m-<?php bp_message_thread_id(); ?>" class="<?php bp_message_css_class(); ?>">
+			
+			<span><?php bp_message_thread_unread_count(); ?></span>
+			
+			<span>
+			
+			<?php  if('sentbox' !== bp_current_action()): ?>
+				<a href=""><?php _e( 'From', 'buddypress' ); ?> <?php bp_message_thread_from() ?></a>
+			<?php else: ?>
+				<a href=""><?php _e( 'To', 'buddypress' ); ?> <?php bp_message_thread_to() ;?></a>
+			<?php endif; ?>
+			
+			</span>
+			
+			<a href="<?php bp_message_thread_view_link(); ?>"><?php bp_message_thread_subject(); ?></a>
+		
+		</li>
+
+	<?php endwhile; ?>	
+	
+	</ul>
+
+</div><!-- / .messages-list -->
+
+<?php endif; ?>
