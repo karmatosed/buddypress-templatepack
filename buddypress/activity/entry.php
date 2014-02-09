e5b1e9cdd662d0997d45e4316dde901c878380d1
Boone B Gorges <boonebgorges@gmail.com>
2014-02-09 14:26:59 -0500

    Put ac-form outside of if ( comment_count() ) block
    Otherwise it's only possible to comment on a post that has existing comments
    
    See #60

--- buddypress/activity/entry.php
+++ buddypress/activity/entry.php
@@ -58,24 +58,23 @@
 					<?php bp_activity_comments_user_avatars(); ?>
 				</ul>
 				<?php bp_activity_comments(); ?>
-				<?php if ( is_user_logged_in() ) : ?>
-
-					<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
-						<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
-						<div class="ac-reply-content">
-							<div class="ac-textarea">
-								<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
-							</div>
-							<input type="submit" name="ac_form_submit" value="<?php _e( 'Post', 'buddypress' ); ?>" /> &nbsp; <a href="#" class="ac-reply-cancel"><?php _e( 'Cancel', 'buddypress' ); ?></a>
-							<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
-						</div>
-						<?php do_action( 'bp_activity_entry_comments' ); ?>
-						<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
-					</form>
-
-				<?php endif; ?>
 
 			</div>
+		<?php endif; ?>
+
+		<?php if ( ( is_user_logged_in() && bp_activity_can_comment() ) || bp_is_single_activity() ) : ?>
+			<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
+				<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
+				<div class="ac-reply-content">
+					<div class="ac-textarea">
+						<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
+					</div>
+					<input type="submit" name="ac_form_submit" value="<?php _e( 'Post', 'buddypress' ); ?>" /> &nbsp; <a href="#" class="ac-reply-cancel"><?php _e( 'Cancel', 'buddypress' ); ?></a>
+					<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
+				</div>
+				<?php do_action( 'bp_activity_entry_comments' ); ?>
+				<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
+			</form>
 		<?php endif; ?>
 
 		<?php do_action( 'bp_after_activity_entry_comments' ); ?>
