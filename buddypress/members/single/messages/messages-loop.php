4cd785e5911686e7c61dcb88299248e4d938c8a1
Trisha Salas <trisha@trishasalas.com>
2014-03-02 18:15:11 -0600

    Adding back in ajax/compose code for testing

--- buddypress/members/single/messages/messages-loop.php
+++ buddypress/members/single/messages/messages-loop.php
@@ -7,66 +7,65 @@
  */
 ?>
 
+<button>Compose</button>
+
 <?php do_action( 'bp_before_member_messages_loop' ); ?>
 
 <?php if ( bp_has_message_threads( bp_ajax_querystring( 'messages' ) ) ) : ?>
 
-	<button>Compose</button>
-
 <div class="messages-list">
 
-	<?php 	$i=0; 
-			while ( bp_message_threads() ) : bp_message_thread(); 
-			$i++;
-		if ($i < 2) {
-			$class = "show";
-			} else {
-			$class = "hide";
+	<?php
+	$i=0;
+	while ( bp_message_threads() ) : bp_message_thread();
+	$i++;
+	if ($i < 2) {
+		$class = "show";
+	} else {
+		$class = "hide";
 		}?>
-
-		<div class="message-wrap">
-			<div id="m-<?php bp_message_thread_id(); ?>" class="message-header <?php bp_message_css_class(); ?>">
-			<!--<?php if ( bp_message_thread_has_unread() ) : ?>unread<?php else: ?>read<?php endif; ?>-->
-			<?php bp_message_thread_avatar(); ?>
-			<?php if( bp_message_thread_has_unread() ) : ?>
-			<span class="unread-num"><?php bp_message_thread_unread_count(); ?> <?php _e( 'Unread', 'buddypress' ); ?></span>
-			<?php endif; ?>
-
-			<?php  if('sentbox' !== bp_current_action()): ?>
-			<span><?php _e( 'From: ', 'buddypress' ); ?> <?php bp_message_thread_from() ?></span>
-			<?php endif; ?>
-			<?php bp_message_thread_subject(); ?>
-		</div>
-
-		<div class="message-content">
-		<div class="show-<?php bp_message_thread_id(); ?>">
-<<<<<<< Updated upstream
-		<strong><?php bp_message_thread_subject(); ?> - <?php bp_message_thread_last_post_date(); ?></strong>
-=======
-		<?php bp_message_thread_last_post_date(); ?>
->>>>>>> Stashed changes
-		</div>
-		
-		<div id="content-<?php bp_message_thread_id(); ?>" class="<?php echo $class;?>">
-		<?php bp_message_thread_content();?>
-		</div>
-		</div>
+ 
+        <div class="message-wrap">
+            <div id="m-<?php bp_message_thread_id(); ?>" class="message-header <?php bp_message_css_class(); ?>">
+            <!--<?php if ( bp_message_thread_has_unread() ) : ?>unread<?php else: ?>read<?php endif; ?>-->
+            <?php bp_message_thread_avatar(); ?>
+            <?php if( bp_message_thread_has_unread() ) : ?>
+            <span class="unread-num"><?php bp_message_thread_unread_count(); ?> <?php _e( 'Unread', 'buddypress' ); ?></span>
+            <?php endif; ?>
+ 
+            <?php  if('sentbox' !== bp_current_action()): ?>
+            <span><?php _e( 'From: ', 'buddypress' ); ?> <?php bp_message_thread_from() ?></span>
+            <?php else: ?>
+            <span><?php _e( 'To: ', 'buddypress' ); ?> <?php bp_message_thread_to() ;?></span>
+            <?php endif; ?>
+            <?php bp_message_thread_subject(); ?>
+        </div>
+ 
+        <div class="message-content">
+        <div class="show-<?php bp_message_thread_id(); ?>">
+        <strong><?php bp_message_thread_subject(); ?> - <?php bp_message_thread_last_post_date(); ?></strong>
+        </div>
+         
+        <div id="content-<?php bp_message_thread_id(); ?>" class="<?php echo $class;?>">
+        <?php bp_message_thread_content();?>
+        </div>
+        </div>
 </div>
-
+ 
 <script>
-	jQuery(document).ready(function($) {
-  		$(".show-<?php bp_message_thread_id(); ?>").click(function(){
-    	$("#content-<?php bp_message_thread_id(); ?>").toggle( 500 );
-		});
-	});
-</script>
-<script>
-	jQuery(document).ready(function($) {
-		$("button").click(function(){
-  $(".message-wrap").load("http://local.wordpress.dev/buddypress/compose-message");
-});
-});
+    jQuery(document).ready(function($) {
+        $(".show-<?php bp_message_thread_id(); ?>").click(function(){
+        $("#content-<?php bp_message_thread_id(); ?>").toggle( 500 );
+        });
+    });
 </script>
 <?php endwhile; ?>
 </div><!-- / .messages-list -->
 <?php endif; ?>
+<script>
+	jQuery(document).ready(function($) {
+		$("button").click(function(){
+  $(".messages-list").load("http://local.wordpress.dev/members/admin/messages/compose");
+});
+});
+</script>
