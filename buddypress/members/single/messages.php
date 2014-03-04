e21c8f5c0317ffb63900fad44dbe4b9bb3bbd214
Boone B Gorges <boonebgorges@gmail.com>
2014-03-04 10:45:15 -0500

    Attempting to make sense of two-panel messages view

--- buddypress/members/single/messages.php
+++ buddypress/members/single/messages.php
@@ -24,45 +24,72 @@
 
 </div><!-- .item-list-tabs -->
 
-<?php
-switch ( bp_current_action() ) :
+<div class="messages-sidebar">
+	<?php if ( bp_has_message_threads( bp_ajax_querystring( 'messages' ) ) ) : ?>
+		<?php while ( bp_message_threads() ) : bp_message_thread(); ?>
+		<div class="message-wrap">
+			<div id="m-<?php bp_message_thread_id(); ?>" class="message-header <?php bp_message_css_class(); ?>">
+				<?php bp_message_thread_avatar() ?>
 
-	// Inbox/Sentbox
-	case 'inbox'   :
-	case 'sentbox' :
-		do_action( 'bp_before_member_messages_content' ); ?>
+				<?php if ( bp_message_thread_has_unread() ) : ?>
+					<span class="unread-num"><?php printf( __( '%s Unread', 'buddypress' ), bp_get_message_thread_unread_count() ); ?></span>
+				<?php endif; ?>
+
+				<?php if ( bp_is_current_action( 'sentbox' ) ) : ?>
+					<span><?php _e( 'To: ', 'buddypress' ); ?> <?php bp_message_thread_to(); ?></span>
+				<?php else : ?>
+					<span><?php _e( 'From: ', 'buddypress' ); ?> <?php bp_message_thread_from(); ?></span>
+				<?php endif; ?>
+
+				<p><a href="<?php bp_message_thread_view_link(); ?>" title="<?php esc_attr_e( "View Message", "buddypress" ); ?>"><?php bp_message_thread_subject(); ?></a></p>
+			</div>
+		</div>
+		<?php endwhile; ?>
+	<?php else : ?>
+		<?php _e( 'No messages', 'buddypress' ); ?>
+	<?php endif; ?>
+</div>
+
+<div class="messages-main">
+	<?php
+	switch ( bp_current_action() ) :
+
+		// Inbox/Sentbox
+		case 'inbox'   :
+		case 'sentbox' :
+			// THIS IS A HACK - will require a BuddyPress patch
+			global $messages_template;
+			buddypress()->current_action = 'view';
+			buddypress()->action_variables[0] = intval( $messages_template->threads[0]->thread_id );
+
+			do_action( 'bp_before_member_messages_content' );
+			bp_get_template_part( 'members/single/messages/single' );
+			do_action( 'bp_after_member_messages_content' );
+			break;
+
+		// Single Message View
+		case 'view' :
+			bp_get_template_part( 'members/single/messages/single' );
+			break;
+
+		// Compose
+		case 'compose' :
+			bp_get_template_part( 'members/single/messages/compose' );
+			break;
+
+		// Sitewide Notices
+		case 'notices' :
+			do_action( 'bp_before_member_messages_content' ); ?>
 
 
-			<?php 
-			
-			bp_get_template_part( 'members/single/messages/messages-loop' ); ?>
+				<?php bp_get_template_part( 'members/single/messages/notices-loop' );; ?>
 
+			<?php do_action( 'bp_after_member_messages_content' );
+			break;
 
-		<?php do_action( 'bp_after_member_messages_content' );
-		break;
-
-	// Single Message View
-	case 'view' :
-		bp_get_template_part( 'members/single/messages/single' );
-		break;
-
-	// Compose
-	case 'compose' :
-		bp_get_template_part( 'members/single/messages/compose' );
-		break;
-
-	// Sitewide Notices
-	case 'notices' :
-		do_action( 'bp_before_member_messages_content' ); ?>
-
-
-			<?php bp_get_template_part( 'members/single/messages/notices-loop' );; ?>
-
-		<?php do_action( 'bp_after_member_messages_content' );
-		break;
-
-	// Any other
-	default :
-		bp_get_template_part( 'members/single/plugins' );
-		break;
-endswitch;
+		// Any other
+		default :
+			bp_get_template_part( 'members/single/plugins' );
+			break;
+	endswitch; ?>
+</div>
