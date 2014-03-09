fde21db35a19011bc95be19d99be4f59a401b5af
Tammie Lister <karmatosed@gmail.com>
2014-03-09 18:58:23 +0000

    Activity comments fix
    The activity comments weren’t hiding and showing.
    This is now resolved so they reveal on click

--- buddypress/activity/entry.php
+++ buddypress/activity/entry.php
@@ -51,40 +51,32 @@
 						<li class="activity-delete"><?php bp_activity_delete_link(); ?></li>
 					<?php endif; ?>
 				</ul>
+				<?php do_action( 'bp_activity_entry_meta' ); ?>
 			<?php endif; ?>
-			<?php do_action( 'bp_activity_entry_meta' ); ?>
 		</div>
 
 		<?php do_action( 'bp_before_activity_entry_comments' ); ?>
-		<?php if ( bp_activity_get_comment_count() ) : ?>
-			<div class="activity-comments">
-				<p class="activity-comments-label"><?php printf( _n( 'There is %s voice in this conversation &nbsp;', 'There are %s voices in this conversation &nbsp;', count( bp_activity_get_comments_user_ids() ), 'buddypress' ), number_format_i18n( count( bp_activity_get_comments_user_ids() ) ) ); ?></p>
-				<ul class="activity-coments-avatars">
-					<?php bp_activity_comments_user_avatars(); ?>
-				</ul>
-				<?php bp_activity_comments(); ?>
-
-			</div>
-		<?php endif; ?>
 
 		<?php if ( ( is_user_logged_in() && bp_activity_can_comment() ) || bp_is_single_activity() ) : ?>
-			<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
-				<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
-				<div class="ac-reply-content">
-					<div class="ac-textarea">
-						<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
-					</div>
-					<input type="submit" name="ac_form_submit" value="<?php _e( 'Post', 'buddypress' ); ?>" /> &nbsp; <a href="#" class="ac-reply-cancel"><?php _e( 'Cancel', 'buddypress' ); ?></a>
-					<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
-				</div>
-				<?php do_action( 'bp_activity_entry_comments' ); ?>
-				<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
-			</form>
+			<div class="activity-comments">
+				<?php bp_activity_comments(); ?>
+				<?php if ( is_user_logged_in() ) : ?>
+					<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
+						<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
+						<div class="ac-reply-content">
+							<div class="ac-textarea">
+								<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
+							</div>
+							<input type="submit" name="ac_form_submit" value="<?php _e( 'Post', 'buddypress' ); ?>" /> &nbsp; <a href="#" class="ac-reply-cancel"><?php _e( 'Cancel', 'buddypress' ); ?></a>
+							<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
+						</div>
+						<?php do_action( 'bp_activity_entry_comments' ); ?>
+						<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
+					</form>
+				<?php endif; ?>
+			</div>
 		<?php endif; ?>
-
 		<?php do_action( 'bp_after_activity_entry_comments' ); ?>
-
-	</div>
 </li>
 
 <?php do_action( 'bp_after_activity_entry' ); ?>
