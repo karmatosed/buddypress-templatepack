10b3360315e40f6c3df2d929ade6c415a2ee44da
hnla <hugoashmore@tiscali.co.uk>
2014-03-13 20:51:03 +0000

    Kick group activity filter into life, human readable re-factoring group/single/activity.php #78

--- buddypress/groups/single/activity.php
+++ buddypress/groups/single/activity.php
@@ -7,14 +7,19 @@
  */
 ?>
 <nav id="sub-nav" class="item-list-tabs no-ajax" role="navigation">
+
 	<ul>
+
 		<li class="feed"><a href="<?php bp_group_activity_feed_link(); ?>" title="<?php _e( 'RSS Feed', 'buddypress' ); ?>"><?php _e( 'RSS', 'buddypress' ); ?></a></li>
 
 		<?php do_action( 'bp_group_activity_syndication_options' ); ?>
 
-		<li id="activity-filter" class="last">
+		<li id="activity-filter-select" class="last">
+
 			<label for="activity-filter-by"><?php _e( 'Show:', 'buddypress' ); ?></label>
+
 			<select id="activity-filter-by">
+
 				<option value="-1"><?php _e( 'Everything', 'buddypress' ); ?></option>
 				<option value="activity_update"><?php _e( 'Updates', 'buddypress' ); ?></option>
 
@@ -26,9 +31,13 @@
 				<option value="joined_group"><?php _e( 'Group Memberships', 'buddypress' ); ?></option>
 
 				<?php do_action( 'bp_group_activity_filter_options' ); ?>
+
 			</select>
+
 		</li>
+
 	</ul>
+
 </nav>
 
 <?php do_action( 'bp_before_group_activity_post_form' ); ?>
@@ -40,6 +49,7 @@
 <?php endif; ?>
 
 <?php do_action( 'bp_after_group_activity_post_form' ); ?>
+
 <?php do_action( 'bp_before_group_activity_content' ); ?>
 
 <div id="group-activity" class="activity single-group">
