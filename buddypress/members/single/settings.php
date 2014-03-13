47317885e18a01d89821e916c4b6f6544d1ae305
r-a-y <r-a-y@gmx.com>
2014-03-12 18:21:35 -0700

    Add support for the new "Settings > Profile" page.

--- buddypress/members/single/settings.php
+++ buddypress/members/single/settings.php
@@ -32,6 +32,8 @@ switch ( bp_current_action() ) :
 	case 'general'        :
 		bp_get_template_part( 'members/single/settings/general'        );
 		break;
+	case 'profile'        :
+		bp_get_template_part( 'members/single/settings/profile'        );
 	default:
 		bp_get_template_part( 'members/single/plugins'                 );
 		break;
